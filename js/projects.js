32cd913b98ec27669297fa4b7324d7fc513f12dd
unknown <lorenzohpanato@gmail.com>
2023-06-03 15:03:59 -0300

    second commit

---
+++ js/projects.js
@@ -0,0 +1,12 @@
+const projectsData = [
+    {
+        image: 'assets/images/todo.PNG',
+        title: 'Todo List',
+        text: 'Projeto básico desenvolvido com HTML, CSS e JavaScript, com o objetivo de ' +
+        'praticar, principalmente, a manipulação de formulários, uso do LocalStorage, filtragem e ' +
+        'pesquisa de itens.',
+    }
+];
+
+const projectsContainer = document.querySelector('.projects-container');
+
